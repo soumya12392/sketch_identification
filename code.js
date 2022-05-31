9b9551090719534e9c1e862cfe94ff61617651a7
Soumya Singh <83289414+soumya12392@users.noreply.github.com>
2022-05-31 16:33:27 +0530

    Add files via upload
    Completed Sketch Identification

--- code.js
+++ code.js
@@ -2,8 +2,40 @@ function setup() {
     var canvas = createCanvas(300, 300);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload() {
+    classifier = ml5.imageClassifier("DoodleNet");
+}
+
+function draw() {
+    strokeWeight(15);
+    stroke(25, 43, 75);
+
+    if (mouseIsPressed) {
+        line(pmouseX, pmouseY, mouseX, mouseY);
+    }
 }
 
 function clearCanvas() {
     background("white");
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+
+    document.getElementById("label").innerHTML = "Label: " + results[0].label;
+    document.getElementById("confidence").innerHTML = "Confidence: " + Math.round(results[0].confidence * 100) + "%";
+
+    var utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
 }
